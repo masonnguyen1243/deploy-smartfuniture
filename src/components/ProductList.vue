b1ef7428a1898bb3bd555ee0eb7dd59ad51f7b9d
xepo-cuongnguyen <masonnguyen1243@gmail.com>
2025-08-10 14:30:09 +0700

    fix

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -40,8 +40,10 @@ onMounted(fetchProducts);
   <div
     class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-8"
   >
-    <RouterLink v-for="(product, index) in products" :key="index">
-      <ProductCard v-bind="product" />
-    </RouterLink>
+    <ProductCard
+      v-for="(product, index) in products"
+      :key="index"
+      v-bind="product"
+    />
   </div>
 </template>
